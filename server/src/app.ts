e7baf9a0b90a844d729b6ba026f67b7cb1ba4845
Purya Zarchiny <puryazarchiny@gmail.com>
2024-12-14 04:50:59 +0330

    chore: add .env

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,10 @@
 import express from "express";
 
+import config from "./config/index.js";
 import postRouter from "./routers/post.js";
 
 const app = express();
-const PORT = 3000;
+const PORT = config.PORT;
 
 /**
  * Routers
